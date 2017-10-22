78aa90dfa00dcb7b2a1fc111b569d80c4e52636b
Peter Prokop <pitrowech@gmail.com>
2017-10-22 13:01:23 +0100

    Initial Commit

---
+++ Evolve/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Evolve
+//
+//  Created by Peter Prokop on 22/10/2017.
+//  Copyright © 2017 Peter Prokop. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
